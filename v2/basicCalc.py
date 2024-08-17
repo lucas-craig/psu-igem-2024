018bb602903d1d2d386064ef8b8cb94ca8465f6e
Lucas Craig <lucasc@M4cB00k-Pr0.local>
2024-08-17 19:28:29 -0400

    cleaned up comments

--- v2/basicCalc.py
+++ v2/basicCalc.py
@@ -1,5 +1,4 @@
 import wx
-#import wx.richtext
 import numpy as np
 import scipy as sp
 import conversionBackend as be
@@ -9,8 +8,6 @@ sizer = wx.BoxSizer(wx.VERTICAL)
 
 class BasicCalculator(wx.Frame): #is the class really necessary?
     
-    #input = (False, False)
-    #bfore_first_button_press = True
     text1 = None
     text2 = None
     outTextBox = None
@@ -21,30 +18,16 @@ class BasicCalculator(wx.Frame): #is the class really necessary?
 
         #Create a panel to put all the buttons and text boxes
         panel = wx.Panel(self, style=wx.SUNKEN_BORDER)
-        panel.SetBackgroundColour((195,216,234))
+        panel.SetBackgroundColour((254,245,227))
         
         wx.TopLevelWindow.Center(self, wx.BOTH) #center window when it opens
-        #wx.TopLevelWindow.SetSizeHints(self, 450, 300, maxW=600, maxH=400) #setting min and max heights
         wx.TopLevelWindow.SetSizeHints(self, 450, 300, maxW=500, maxH=330)
-        
 
         #setting up the sizer. 
         vertical_layout = wx.BoxSizer(wx.VERTICAL)
-        #sizer.Add(wx.Button(self, -1, 'An extremely long button text'), 0, 0, 0)
-        #sizer.Add(wx.Button(self, -1, 'Small button'), 0, 0, 0)
-        #self.SetSizer(sizer)
-        #panel.SetSizer(sizer)
-
-        #wx.Panel(self)
-
-        #panel = wx.Panel(self)
-        #self.SetSizerAndFit(self, sizer, deleteOld=True)
 
         # Setting up the menu.
         filemenu= wx.Menu()
-
-        #btn2 = buttons.GenButton(panel, -1, "Hello World!", pos=(50, 100))
-        #btn2 = buttons.GenButton(panel, -1, "Hello World!")
 
         # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
         filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
@@ -54,139 +37,46 @@ class BasicCalculator(wx.Frame): #is the class really necessary?
         # Creating the menubar.
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
-        #self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-        #self.Show(True)
 
-        #Creating a title
-        #st = wx.StaticText(panel, label="Basic Calculator", style=wx.ALIGN_CENTRE_HORIZONTAL)
-        #st = wx.StaticText(self, label="Basic Calculator", style=wx.ALIGN_CENTRE_HORIZONTAL)
-        #vertical_layout.Add(st, 0, wx.ALL |  wx.ALIGN_CENTER, 10)
-
-        #heading = wx.richtext.RichTextCtrl(panel, name="Basic Calculator", style=wx.ALIGN_CENTER_HORIZONTAL)
-        #heading_text = "<b><big><span font_family='wx.FONTFAMILY_SWISS'>Basic Calculator</span></big></b>"
-
-        #wx.SetLabelMarkup(heading_text)
         heading = wx.StaticText(panel, label="Basic Calculator", style=wx.ALIGN_CENTER_HORIZONTAL)
-        #heading_font = wx.Font(wx.FontInfo(30).Family(wx.FONTFAMILY_SWISS).Bold())
-        """
-        heading.SetLabelMarkup("<b>&ampBed</b> &ampmp "
-                     "<span foreground='purple'>breakfast</span> "
-                     "available <big>HERE</big>")
-        """
-        #heading.SetLabelMarkup('<p><font color="#FF0000">Red paragraph text</font></p>')
         
         font = wx.Font(30, wx.DEFAULT, wx.MODERN, wx.NORMAL)
         heading.SetFont(font)
         
-        heading.SetForegroundColour((148,6,27))
         heading.SetForegroundColour((125,51,56))
-       
-        #rgb(255, 0, 0)
-
-        #heading.SetLabelMarkup(True)
-
+    
         vertical_layout.Add(heading, 0, wx.ALL | wx.ALIGN_CENTER, 20)
 
-        #creating text entry boxes
-        #text1 = wx.TextCtrl(panel)
         textbox_size = wx.Size(-1,25)
-        #text1 = wx.TextCtrl(panel, size=wx.Size(30,10))
-        #text1 = wx.TextCtrl(panel, size=textbox_size)
+
         self.text1 = wx.TextCtrl(panel)
         self.text1.SetMaxSize(textbox_size)
         self.text1.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_BTNFACE)) 
 
-        #panel.AddChild(text1)
-        #text1.setLabel("Initial Concen. (mmol/L)")
         self.text1.SetHint("Initial Glucose Reading (mg/dL)")
-        #text2 = wx.TextCtrl(self, pos = (50,70))
-        #text2 = wx.TextCtrl(panel)
-        #text2 = wx.TextCtrl(panel, size=textbox_size)
-        #self.text1.Bind()
-        #self.text1.Bind(wx.EVT_TEXT, self.OnTextClick)
-
-
 
         self.text2 = wx.TextCtrl(panel)
         self.text2.SetMaxSize(textbox_size)
         self.text2.SetHint("Final Glucose Reading (mg/dL)")
         self.text2.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_BTNFACE)) 
         
-        #sizer.Add(panel, 1, 0, 0)
-        
-        #sizer.Add(text1, 1, wx.ALIGN_CENTER, 50)
-        #sizer.Add(text2, 1, wx.ALIGN_CENTER, 50)
-        #vertical_layout.Add(text1, 1, wx.SHAPED | wx.ALL | wx.ALIGN_CENTER, border=20)
-        #vertical_layout.Add(text2, 1, wx.SHAPED | wx.ALL | wx.ALIGN_CENTER, border=20)
         vertical_layout.Add(self.text1, 1, wx.ALL | wx.ALIGN_CENTER, border=10)
         vertical_layout.Add(self.text2, 1, wx.ALL | wx.ALIGN_CENTER, border=10)
-        #vertical_layout.Add(text1, 1, wx.ALIGN_CENTER, border=20)
-        #vertical_layout.Add(text2, 1, wx.ALIGN_CENTER, border=20)
-        #vertical_layout.Add(text1, 1, 0, border=20)
-        #vertical_layout.Add(text2, 1, 0, border=20)
-        #vertical_layout.Add(text1, 1, 0, 0)
-        #vertical_layout.Add(text2, 1, 0, 0)
 
         #creating the calculate button
-        #calcButton = wx.Button(self.panel, label="Calculate", pos = (50,100))
-        #calcButton = wx.Button(self, label="Calculate")
-        #calcButton = wx.Button(panel, label="Calculate")
         calcButton = buttons.GenButton(panel, -1, label="Convert")
-        #calcButton.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_BTNFACE))
         calcButton.SetBackgroundColour((28,58,137))
-        #calcButton.SetFont()#OwnColour((28,58,137))
         calcButton.Bind(wx.EVT_BUTTON, self.OnButtonClick)
         vertical_layout.Add(calcButton, 0, wx.ALL | wx.ALIGN_CENTER, 10)
 
-        #creating the output display box:
-        #self.outText = wx.TextCtrl(self.panel, value = "Type in concentrations and press caclulate.", pos = (50,130), size=(200,100), style=wx.TE_READONLY | wx.TE_MULTILINE)
-        #outText = wx.TextCtrl(self, value = "Type in concentrations and press caclulate.", style=wx.TE_READONLY | wx.TE_MULTILINE)
-        #self.outTextBox = wx.TextCtrl(panel, value = self.outMsg, style=wx.TE_READONLY | wx.TE_MULTILINE)
-        #sizer.Add(outText, 1, wx.EXPAND | wx.LEFT | wx.RIGHT, 2)
-        
+        #creating the output display box:       
         self.outMsg = "Type in concentrations and press convert."
         self.outTextBox = wx.TextCtrl(panel, value = self.outMsg, style=wx.TE_READONLY | wx.TE_MULTILINE)
         vertical_layout.Add(self.outTextBox, 1, wx.EXPAND | wx.BOTTOM, 0)
 
-        """
-        if self.bfore_first_button_press:
-            self.outMsg = "Type in concentrations and press caclulate."
-
-        elif (not self.input[0] or not self.input[1]): #self.input[0] == False or self.input[1] == False:
-            self.outMsg = "Error: a glucose concentration is out of specified glucometer's measurement range."
-
-        elif not (self.input[0] >= 20 and self.input[0] <= 500) or not (self.input[1] >= 20 and self.input[1] <= 500):
-                
-                c = be.converter()
-                interpolatedVal = c.convertConcentration(input[0],input[1])
-                self.outMsg = "CA125: {:.3f} U/mL\n".format(interpolatedVal)
-                if interpolatedVal >= 35: 
-                    self.outMsg += "Abnormal: greater than or equal to 35"
-                else: 
-                    self.outMsg += "Normal: below 35"
-        
-        
-        else:
-            self.outMsg = "Something went wrong"
-        self.outText.SetValue(outMsg)
-        #self.UpdateOutputTextbox()
-        """
-
         panel.SetSizer(vertical_layout)
 
-        
 
-    #def UpdateOutputTextbox(self):
-    #    self.outTextBox.SetValue(self.outMsg)
-
-    #id = EVT_TEXT_URL (??)
-    """
-    def OnTextClick(self, e):
-        #source = e.Source()
-        obj = e.GetEventObject()
-        print(obj.GetName())
-        return
-    """
 
     #button press event handler
     def OnButtonClick(self, e):
@@ -199,9 +89,6 @@ class BasicCalculator(wx.Frame): #is the class really necessary?
         contents2 = self.text2.GetValue().strip(' ')
 
         #if there are non-numeric chars in input strings, return false.
-        #if not((contents1.isnumeric()) and (contents2.isnumeric())):
-        #   self.outMsg = "Error: only numeric values are accepted."
-
         if not((contents1.isnumeric()) and (contents2.isnumeric())):
            self.outMsg = "Error: only numeric values are accepted."
         
@@ -226,43 +113,8 @@ class BasicCalculator(wx.Frame): #is the class really necessary?
                 else:
                     self.outMsg = "CA125: {:.3f} U/mL\n".format(interpolatedVal)
                     self.outMsg += "Abnormal: greater than or equal to 35" if interpolatedVal >= 35 else "Normal: below 35"
-
-                """
-                self.outMsg = "CA125: {:.3f} U/mL\n".format(interpolatedVal)
-                if interpolatedVal >= 35: 
-                    self.outMsg += "Abnormal: greater than or equal to 35"
-                else: 
-                    self.outMsg += "Normal: below 35"
-                """
         
         self.outTextBox.SetValue(self.outMsg)
-        #self.UpdateOutputTextbox()
-
-        """
-        #if there are non-numeric chars in input strings, output error message.
-        if not((contents1.isnumeric()) and (contents2.isnumeric())):
-           #print(contents1.isnumeric())
-           #print(contents2.isnumeric())
-           outMsg = "Error: only numeric values are accepted."
-        
-        #otherwise, extract values and run the calculation
-        else:
-            val1 = float(contents1) #extract num from contents of first text box as float
-            val2 = float(contents2) #ditto for second text box
-
-            if not (20 <= val1 <= 500) or not (20 <= val2 <= 500):
-                outMsg = "Error: a glucose concentration is out of specified glucometer measurement range."
-
-
-            c = be.converter()
-            interpolatedVal = c.convertConcentration(contents1,contents2)
-            outMsg = "CA125: {:.3f} U/mL\n".format(interpolatedVal)
-            if interpolatedVal >= 35: 
-                outMsg += "Abnormal: greater than or equal to 35"
-            else: 
-                outMsg += "Normal: below 35"
-        self.outText.SetValue(outMsg)
-        """
         
 
     def makeMenuBar(self):
@@ -287,10 +139,6 @@ class BasicCalculator(wx.Frame): #is the class really necessary?
         helpMenu = wx.Menu()
         aboutItem = helpMenu.Append(wx.ID_ABOUT)
 
-        # Make the menu bar and add the two menus to it. The '&' defines
-        # that the next letter is the "mnemonic" for the menu item. On the
-        # platforms that support it those letters are underlined and can be
-        # triggered from the keyboard.
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, "&File")
         menuBar.Append(helpMenu, "&Help")
